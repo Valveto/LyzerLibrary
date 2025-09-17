443968481b24e637ad0d292cf5e35247fe3bffb1
Valveto <kazeruyn@gmail.com>
2025-09-17 16:13:57 -0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -2430,20 +2430,20 @@ function RayfieldLibrary:CreateWindow(Settings)
 			Section.Title.Text = SectionSettings.Name
 			Section.Visible = true
 			Section.Parent = TabPage
-				print("n")
+				print("c")
 			local function GetLineSize(text)
 				local baseSize = 0.83   -- tamanho máximo da linha
 				local minSize = 0.15    -- tamanho mínimo da linha
 				local baseChars = 6     -- referência de caracteres
 
-				local charCount = #text
-				-- Fórmula suave usando inverso + leve logaritmo para não diminuir rápido demais
-				local sizeX = baseSize / (1 + math.log(charCount / baseChars + 1))
+				local charCount = math.max(#text, baseChars)  -- evita que linhas cresçam para textos curtos
+				-- Fórmula suave usando logaritmo
+				local sizeX = baseSize / (1 + math.log(charCount / baseChars))
 
-				-- Garantir que fique dentro do mínimo e máximo
 				sizeX = math.clamp(sizeX, minSize, baseSize)
 				return sizeX
 			end
+
 
 			local lineSizeX = GetLineSize(Section.Title.Text)
 

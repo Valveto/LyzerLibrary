1b31810cbeb0216ea504b7b45ea33ccc707e2d48
Valveto <kazeruyn@gmail.com>
2025-09-17 16:53:28 -0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -2434,7 +2434,7 @@ function RayfieldLibrary:CreateWindow(Settings)
 			local function GetLineSize(text)
 				local baseSize = 0.83 
 				local minSize = 0.15
-				local baseChars = 0 
+				local baseChars = 5
 
 				if #text <= baseChars then
 					return baseSize 
